8f2404a80163c6956fe2371fdb116725f088da53
Michael J <johnson_michael1329@yahoo.com>
2022-03-16 09:05:17 -0500

    refactored my original code for the day trip generator

--- DayTripGenerator/main.py
+++ DayTripGenerator/main.py
@@ -9,66 +9,36 @@ def day_trip_generator():
     users_name = input("Enter your name for a chance to take a trip: ")
     print(f"Hello {users_name}, you have been selected. Let's create your itinerary!")
 
-    def users_city():
-        users_do_not_like_city = True
-        while users_do_not_like_city == True:
-             yes_or_no = input(f"Would you like to go to {random.choice(tourist_destinations)}? y or n?: ")
-             if yes_or_no == "y":
-                users_do_not_like_city = False
-                users_city_choice = random.choice(tourist_destinations)
-                print(f"Awesome! You will be heading to {users_city_choice}!")
-                return users_city_choice
-             users_do_not_like_city = True
-    city_choice = users_city()
+    def trip_lists(list_1):
+      do_not_like_city = True
 
-    def users_transportation():
-        users_do_not_like_transportation = True       
-        while users_do_not_like_transportation == True:
-             yes_or_no = input(f"Would you like to travel by {random.choice(transportation_type)}? y or n?: ")
-             if yes_or_no == "y":
-                users_do_not_like_transportation = False
-                users_transportation_choice = random.choice(transportation_type)
-                print(f"Great choice! You will be traveling by {random.choice(transportation_type)}!")
-                return users_transportation_choice
-    transportation_choice = users_transportation()       
-            
-    def users_food():
-        users_do_not_like_restuarant = True
-        while users_do_not_like_restuarant == True:
-             yes_or_no = input(f"Would you like to eat at the {random.choice(restaurant_types)} restuarant? y or n?: ")
-             if yes_or_no == "y":
-                users_do_not_like_restuarant = False
-                users_food_choice = random.choice(restaurant_types)
-                print(f"Stellar! You will be eating at the {random.choice(restaurant_types)} restaruant!")
-                return users_food_choice
-    food_choice = users_food()
+      while do_not_like_city == True:
+          random_list_1 = random.choice(list_1)
+          yes_or_no = input(f"What do you like? {random_list_1}? y or n?: ")
+          if yes_or_no.lower() == "y":
+            do_not_like_city = False
+            print(f"Awesome! You selected {random_list_1}!")
+            return random_list_1
+      
+    users_destination = trip_lists(tourist_destinations)
+    users_entertainment = trip_lists(entertainment_type)
+    users_transportation = trip_lists(transportation_type)
+    users_food = trip_lists(restaurant_types)
+    
+    like_choices = True
 
-    def users_entertainment():
-        users_do_not_like_entertainment = True
-        while users_do_not_like_entertainment == True:
-             yes_or_no = input(f"Do you like the {random.choice(entertainment_type)}? y or n?: ")
-             if yes_or_no == "y":
-                users_do_not_like_entertainment = False
-                users_entertainment_choice = random.choice(entertainment_type)
-                print(f"Fantastic! {random.choice(entertainment_type)} will be added to your itinerary!")
-                return users_entertainment_choice
-    entertainment_choice = users_entertainment()
+    while like_choices == True:
+      yes_or_no = input(f"Do you like your choices: {users_food}, {users_destination}, {users_entertainment}, {users_transportation}? y or n?: ") 
+      if yes_or_no.lower() == "y":
+        like_choices = False 
+        print(f'Congrats on your choices {users_food}, {users_destination}, {users_entertainment}, {users_transportation}!')
+      else:
+          print("Our apologies, lets try again")
+          users_destination = trip_lists(tourist_destinations)
+          users_entertainment = trip_lists(entertainment_type)
+          users_transportation = trip_lists(transportation_type)
+          users_food = trip_lists(restaurant_types)    
 
-    def finalize_trip():
-        print(f'Here is what you selected; City: {city_choice}, Transportation: {transportation_choice}, Food: {food_choice}, and Entertainment: {entertainment_choice}.')
-        yes_or_no = input(f"Do you like your itinerary? y or n?: ")
-        if yes_or_no == "y":
-            print(f'Congrats {users_name}, you will be heading to {city_choice} next month. {transportation_choice} will be your method of Travel. You will have a free meal at the {food_choice} restuarant. Enjoy your night at the {entertainment_choice}')
-            return
-        print(f"My apologies {users_name}, lets try this one more time!")
-        updated_city = users_city()
-        updated_transportation = users_transportation()
-        updated_food = users_food()
-        updated_entertainment = users_entertainment()
-        print(f'Congrats {users_name}, you will be heading to {updated_city} next month. {updated_transportation} will be your method of Travel. You will have a free meal at the {updated_food} restuarant. Enjoy your night at the {updated_entertainment}')
-        return
-    finalize_trip() 
 
 day_trip_generator()
-
 
--- DayTripGenerator/refactor_main.py
+++ DayTripGenerator/refactor_main.py
@@ -1,6 +1,7 @@
 
-import random
 
+
+import random
 
 def day_trip_generator():
     tourist_destinations = ["Memphis", "Houston", "San Francisco", "Boston"]
@@ -11,28 +12,9 @@ def day_trip_generator():
     users_name = input("Enter your name for a chance to take a trip: ")
     print(f"Hello {users_name}, you have been selected. Let's create your itinerary!")
 
-
-
-
-    def trip_lists(lists):
-        users_do_not_like_city = True
-
-        while users_do_not_like_city == True:
-            yes_or_no = input(f"What do you like? {random.choice(lists)}? y or n?: ")
-            if yes_or_no == "y":
-                users_do_not_like_city = False
-                random.choice(lists)
-                print(f"Awesome! You selected {city}!")
-
-           
-
-
-
     def users_city():
         users_do_not_like_city = True
-        
         while users_do_not_like_city == True:
-    
              yes_or_no = input(f"Would you like to go to {random.choice(tourist_destinations)}? y or n?: ")
              if yes_or_no == "y":
                 users_do_not_like_city = False
@@ -42,61 +24,52 @@ def day_trip_generator():
              users_do_not_like_city = True
     city_choice = users_city()
 
-    print(f'Awesome youre going to {city_choice}')
-    # def users_transportation():
-    #     users_do_not_like_transportation = True
-        
-    #     while users_do_not_like_transportation == True:
-    #         for travel_method in transportation_type:
-    #          yes_or_no = input(f"Would you like to travel by {travel_method.lower()}? y or n?: ")
-    #          if yes_or_no == "y":
-    #             users_do_not_like_transportation = False
-    #             users_transportation_choice = travel_method
-    #             print(f"Great choice! You will be traveling by {travel_method.lower()}!")
-    #             return users_transportation_choice
-    # transportation_choice = users_transportation()       
+    def users_transportation():
+        users_do_not_like_transportation = True       
+        while users_do_not_like_transportation == True:
+             yes_or_no = input(f"Would you like to travel by {random.choice(transportation_type)}? y or n?: ")
+             if yes_or_no == "y":
+                users_do_not_like_transportation = False
+                users_transportation_choice = random.choice(transportation_type)
+                print(f"Great choice! You will be traveling by {random.choice(transportation_type)}!")
+                return users_transportation_choice
+    transportation_choice = users_transportation()       
             
-    # def users_food():
-    #     users_do_not_like_restuarant = True
-        
-    #     while users_do_not_like_restuarant == True:
-    #         for food in restaurant_types:
-    #          yes_or_no = input(f"Would you like to eat at the {food} restuarant? y or n?: ")
-    #          if yes_or_no == "y":
-    #             users_do_not_like_restuarant = False
-    #             users_food_choice = food
-    #             print(f"Stellar! You will be eating at the {food} restaruant!")
-    #             return users_food_choice
-    # food_choice = users_food()
+    def users_food():
+        users_do_not_like_restuarant = True
+        while users_do_not_like_restuarant == True:
+             yes_or_no = input(f"Would you like to eat at the {random.choice(restaurant_types)} restuarant? y or n?: ")
+             if yes_or_no == "y":
+                users_do_not_like_restuarant = False
+                users_food_choice = random.choice(restaurant_types)
+                print(f"Stellar! You will be eating at the {random.choice(restaurant_types)} restaruant!")
+                return users_food_choice
+    food_choice = users_food()
 
-    # def users_entertainment():
-    #     users_do_not_like_entertainment = True
-        
-    #     while users_do_not_like_entertainment == True:
-    #         for activity in entertainment_type:
-    #          yes_or_no = input(f"Do you like the {activity}? y or n?: ")
-    #          if yes_or_no == "y":
-    #             users_do_not_like_entertainment = False
-    #             users_entertainment_choice = activity
-    #             print(f"Fantastic! {activity} will be added to your itinerary!")
-    #             return users_entertainment_choice
-    # entertainment_choice = users_entertainment()
+    def users_entertainment():
+        users_do_not_like_entertainment = True
+        while users_do_not_like_entertainment == True:
+             yes_or_no = input(f"Do you like the {random.choice(entertainment_type)}? y or n?: ")
+             if yes_or_no == "y":
+                users_do_not_like_entertainment = False
+                users_entertainment_choice = random.choice(entertainment_type)
+                print(f"Fantastic! {random.choice(entertainment_type)} will be added to your itinerary!")
+                return users_entertainment_choice
+    entertainment_choice = users_entertainment()
 
-    # def finalize_trip():
-    #     print(f'Here is what you selected; City: {city_choice}, Transportation: {transportation_choice}, Food: {food_choice}, and Entertainment: {entertainment_choice}.')
-    #     yes_or_no = input(f"Do you like your itinerary? y or n?: ")
-    #     if yes_or_no == "y":
-    #         print(f'Congrats {users_name}, you will be heading to {city_choice} next month. {transportation_choice} will be your method of Travel. You will have a free meal at the {food_choice} restuarant. Enjoy your night at the {entertainment_choice}')
-    #         return
-    #     print(f"My apologies {users_name}, lets try this one more time!")
-    #     updated_city = users_city()
-    #     updated_transportation = users_transportation()
-    #     updated_food = users_food()
-    #     updated_entertainment = users_entertainment()
-    #     print(f'Congrats {users_name}, you will be heading to {updated_city} next month. {updated_transportation} will be your method of Travel. You will have a free meal at the {updated_food} restuarant. Enjoy your night at the {updated_entertainment}')
-    #     return
-    # finalize_trip() 
+    def finalize_trip():
+        print(f'Here is what you selected; City: {city_choice}, Transportation: {transportation_choice}, Food: {food_choice}, and Entertainment: {entertainment_choice}.')
+        yes_or_no = input(f"Do you like your itinerary? y or n?: ")
+        if yes_or_no == "y":
+            print(f'Congrats {users_name}, you will be heading to {city_choice} next month. {transportation_choice} will be your method of Travel. You will have a free meal at the {food_choice} restuarant. Enjoy your night at the {entertainment_choice}')
+            return
+        print(f"My apologies {users_name}, lets try this one more time!")
+        updated_city = users_city()
+        updated_transportation = users_transportation()
+        updated_food = users_food()
+        updated_entertainment = users_entertainment()
+        print(f'Congrats {users_name}, you will be heading to {updated_city} next month. {updated_transportation} will be your method of Travel. You will have a free meal at the {updated_food} restuarant. Enjoy your night at the {updated_entertainment}')
+        return
+    finalize_trip() 
 
 day_trip_generator()
-
-
